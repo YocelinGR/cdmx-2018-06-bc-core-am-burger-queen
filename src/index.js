4d2a175036cfa552b2dbdf0605144b5318e40701
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-10-18 14:45:46 -0500

    Fixing boiler plate for adding components in a correct way to iimplement deploy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Home from './Components/Home/Home';
+import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
