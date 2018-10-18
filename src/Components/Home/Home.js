2db46f60c653bcf3bf3f5b864b6def2e5ecccb33
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-10-18 14:39:21 -0500

    Adding the resources to use material ui as a desigb framework

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+// Keras, pytoch-beca->udacity300 mejores. Redoz y context
+// networkers 
+class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+
+            </div>
+        )
+    }
+}
+
