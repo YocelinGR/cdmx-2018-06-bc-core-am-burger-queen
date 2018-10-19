16b24286cfef2babd5ee95496aea36e772a76f95
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-10-19 12:36:53 -0500

    Adding format to avatar in login

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,8 @@ import {
   Button, Row, Col,
   Input,
 } from 'react-materialize';
-import { Config } from '../../config';
+import { Config } from '../../config'; 
+import './Home.css'
 import firebase from 'firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
@@ -32,12 +33,16 @@ class Home extends Component{
     return(
       <div className= "LogInStyle">
         {this.state.isSignedIn ? (
+        <div>
           <div className = "user-loged">
-            <h3>Hola, hoy serás una mejor versión de ti misma</h3>
-            <h4>Diabeamigo: {firebase.auth().currentUser.displayName}</h4>
-            <img alt = "foto de usuario" src= {firebase.auth().currentUser.photoURL} />
+            <h3>Hola, hoy serás una gran día de trabajo</h3>
+            <h4>Hola: {firebase.auth().currentUser.displayName}</h4>
+            {firebase.auth().currentUser.photoURL===null ?
+            (<img alt = "foto de usuario" src="https://github.com/YocelinGR/cdmx-2018-06-bc-core-am-burger-queen/blob/master/assets/waitress-avatar.jpg?raw=true" />) :
+            (<img alt = "foto de usuario" src= {firebase.auth().currentUser.photoURL} />)}
             <button className = "btn btn-floating pink lighten-1" onClick={() => firebase.auth().signOut()}>Salir</button>
           </div>
+        </div>
         ) : (
           <StyledFirebaseAuth uiConfig= {this.uiConfig} firebaseAuth = {firebase.auth()} />
         )}
