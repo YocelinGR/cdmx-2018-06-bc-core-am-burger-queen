b9d136c6260a6bdaaa363dacaa247c21969f7ca2
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-10-26 09:08:37 -0500

    Adding all the menu on thecalculator price

--- src/Components/GetYourOrder/GetYourOrder.js
+++ src/Components/GetYourOrder/GetYourOrder.js
@@ -50,22 +50,22 @@ class GetYourOrder extends Component {
         <DisplayOrder data={this.state.operations} />
         <Buttons>
           <Button onClick={this.handleClick} label="C" value="clear" />
-          <Button onClick={this.handleClick} label="7" value="7" />
-          <Button onClick={this.handleClick} label="4" value="4" />
-          <Button onClick={this.handleClick} label="1" value="1" />
-          <Button onClick={this.handleClick} label="0" value="0" />
+          <Button onClick={this.handleClick} label="Café A" value="5" />
+          <Button onClick={this.handleClick} label="Café L" value="7" />
+          <Button onClick={this.handleClick} label="Swandich" value="10" />
+          <Button onClick={this.handleClick} label="Jugo" value="7" />
 
-          <Button onClick={this.handleClick} label="/" value="/" />
-          <Button onClick={this.handleClick} label="8" value="8" />
-          <Button onClick={this.handleClick} label="5" value="5" />
-          <Button onClick={this.handleClick} label="2" value="2" />
-          <Button onClick={this.handleClick} label="." value="." />
+          <Button onClick={this.handleClick} label="Hamb S" value="10" />
+          <Button onClick={this.handleClick} label="Ham D" value="15" />
+          <Button onClick={this.handleClick} label="Papas" value="5" />
+          <Button onClick={this.handleClick} label="Onion" value="5" />
+          <Button onClick={this.handleClick} label="Agua C" value="5" />
 
-          <Button onClick={this.handleClick} label="x" value="*" />
-          <Button onClick={this.handleClick} label="9" value="9" />
-          <Button onClick={this.handleClick} label="6" value="6" />
-          <Button onClick={this.handleClick} label="3" value="3" />
-          <Button label="" value="null" />
+          <Button onClick={this.handleClick} label="Agua G" value="8" />
+          <Button onClick={this.handleClick} label="Refresco C" value="7" />
+          <Button onClick={this.handleClick} label="Refresco G" value="10" />
+          <Button onClick={this.handleClick} label="Queso" value="1" />
+          <Button onClick={this.handleClick} label="Huevo" value="1" />
 
           <Button onClick={this.handleClick} label="-" value="-" />
           <Button onClick={this.handleClick} label="+" size="2" value="+" />
